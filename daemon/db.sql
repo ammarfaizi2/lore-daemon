b68cc009bad85eaf307a45e9b028bdb587394769
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2022-07-07 21:26:15 +0700

    daemon: db: Create MySQL DDL for saving messages information
    We will use a MySQL database to save the Telegram message ID
    and email message ID.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ daemon/db.sql
@@ -0,0 +1,37 @@
+-- Adminer 4.7.6 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP TABLE IF EXISTS `emails`;
+CREATE TABLE `emails` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `message_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `message_id` (`message_id`),
+  KEY `created_at` (`created_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+
+DROP TABLE IF EXISTS `tg_emails`;
+CREATE TABLE `tg_emails` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `email_id` bigint unsigned NOT NULL,
+  `chat_id` bigint NOT NULL,
+  `tg_msg_id` bigint unsigned NOT NULL,
+  `created_at` bigint unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `email_id` (`email_id`),
+  KEY `chat_id` (`chat_id`),
+  KEY `tg_msg_id` (`tg_msg_id`),
+  KEY `created_at` (`created_at`),
+  CONSTRAINT `tg_emails_ibfk_2` FOREIGN KEY (`email_id`) REFERENCES `emails` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
+
+
+-- 2022-07-07 14:25:28
