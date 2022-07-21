bf230f9713587775a85fa4f96a197de48ff958b0
Muhammad Rizki <riskimuhammmad1@gmail.com>
2022-07-22 06:29:25 +0700

    daemon: Move the Telegram bot session into the storage directory
    We want to move the Telegram bot session file into the storage file, so
    it makes the work root directory much cleaner.
    
    Signed-off-by: Muhammad Rizki <riskimuhammmad1@gmail.com>
    Link: https://lore.gnuweeb.org/gwml/20220721232938.503-5-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- daemon/run.py
+++ daemon/run.py
@@ -18,7 +18,7 @@ def main():
 	load_dotenv()
 
 	client = Client(
-		"EmailScraper",
+		"storage/EmailScraper",
 		api_id=int(os.getenv("API_ID")),
 		api_hash=os.getenv("API_HASH"),
 		bot_token=os.getenv("BOT_TOKEN"),
