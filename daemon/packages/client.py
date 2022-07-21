310c084fba8f4302a557fecea8c15f8c95f9058a
Muhammad Rizki <riskimuhammmad1@gmail.com>
2022-07-22 06:29:28 +0700

    daemon: Re-design send email message to Telegram
    We want to separate send message function and inherit with the Pyrogram
    Client, so it should makes the code clean and clear.
    
    Signed-off-by: Muhammad Rizki <riskimuhammmad1@gmail.com>
    Link: https://lore.gnuweeb.org/gwml/20220721232938.503-8-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ daemon/packages/client.py
@@ -0,0 +1,64 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2022  Muhammad Rizki <riskimuhammmad1@gmail.com>
+#
+
+from pyrogram import Client
+from pyrogram.enums import ParseMode
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from typing import Union, BinaryIO
+
+
+class DaemonClient(Client):
+	def __init__(self, name: str, api_id: int,
+			api_hash: str, **kwargs):
+		super().__init__(name, api_id,
+				api_hash, **kwargs)
+
+
+	async def send_text_email(
+		self,
+		chat_id: Union[int, str],
+		text: str,
+		reply_to: int,
+		url: str = None,
+		parse_mode: ParseMode = ParseMode.HTML
+	) -> Message:
+		print("[send_text_email]")
+		return await self.send_message(
+			chat_id=chat_id,
+			text=text,
+			reply_to_message_id=reply_to,
+			parse_mode=parse_mode,
+			reply_markup=InlineKeyboardMarkup([
+				[InlineKeyboardButton(
+					"See the full message",
+					url=url
+				)]
+			])
+		)
+
+
+	async def send_patch_email(
+		self,
+		chat_id: Union[int, str],
+		doc: Union[str, BinaryIO],
+		caption: str,
+		reply_to: int,
+		url: str = None,
+		parse_mode: ParseMode = ParseMode.HTML
+	) -> Message:
+		print("[send_patch_email]")
+		return await self.send_document(
+			chat_id=chat_id,
+			document=doc,
+			caption=caption,
+			reply_to_message_id=reply_to,
+			parse_mode=parse_mode,
+			reply_markup=InlineKeyboardMarkup([
+				[InlineKeyboardButton(
+					"See the full message",
+					url=url
+				)]
+			])
+		)
