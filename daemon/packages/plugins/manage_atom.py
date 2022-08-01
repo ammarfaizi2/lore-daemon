b31d5f993f6a7c356f81a61bbcf3f60e673e02f1
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-08-01 07:19:03 +0700

    daemon: Create /add_atom command
    Create /add_atom command for adding the atom url to the database
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20220801001917.992-6-kiizuha@gnuweeb.org
    Cc: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
    Cc: GNU/Weeb Mailing List <gwml@vger.gnuweeb.org>
    [ ammarfaizi2: Remove a trailing whitespace. ]
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ daemon/packages/plugins/manage_atom.py
@@ -0,0 +1,22 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2022  Muhammad Rizki <riskimuhammmad1@gmail.com>
+#
+
+from pyrogram.types import Message
+from pyrogram import filters
+from packages import DaemonClient
+
+@DaemonClient.on_message(
+        filters.command("add_atom") &
+        filters.chat(["kiizuah", "nekoha", -1001673279485])
+)
+async def add_atom_url(c: DaemonClient, m: Message):
+        pass
+        # text = utils.remove_command(m.text)
+        # if not utils.is_atom_url(text):
+        #         return
+
+        # ###
+        # ### TODO: Muhammad Rizki: Add atom url into the database
+        # ###
