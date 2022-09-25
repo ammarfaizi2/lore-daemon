c5c4da9f2af2ed94bfc91fe18ed64e34fa283d39
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-09-26 04:14:18 +0700

    discord: Add success run notice on_ready event
    Add a bot info and success message on the on_ready event.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20220925211427.412-10-kiizuha@gnuweeb.org
    Cc: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
    Cc: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Cc: GNU/Weeb Mailing List <gwml@vger.gnuweeb.org>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- daemon/dscord/gnuweeb/plugins/__init__.py
+++ daemon/dscord/gnuweeb/plugins/__init__.py
@@ -5,7 +5,13 @@
 
 
 from discord.ext import commands
+from .events import Events
+
+
+class Plugins(
+	Events
+): pass
 
 
 async def setup(bot: "commands.Bot"):
-	pass
+	await bot.add_cog(Plugins(bot))
---
+++ daemon/dscord/gnuweeb/plugins/events/__init__.py
@@ -0,0 +1,11 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2022  Muhammad Rizki <kiizuha@gnuweeb.org>
+#
+
+from .on_ready import OnReady
+
+
+class Events(
+	OnReady,
+): pass
