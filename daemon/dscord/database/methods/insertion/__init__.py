e6ec8ad6e9853de6b2d83fe06e485e294a2e289c
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-09-26 04:14:23 +0700

    discord: Add save_atom() in database insertion
    Add save_atom() to save the atom URL into the database.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20220925211427.412-15-kiizuha@gnuweeb.org
    Cc: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
    Cc: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Cc: GNU/Weeb Mailing List <gwml@vger.gnuweeb.org>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ daemon/dscord/database/methods/insertion/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2022  Muhammad Rizki <kiizuha@gnuweeb.org>
+#
+
+
+from .insert_atom import InsertAtom
+
+
+class Insertion(
+	InsertAtom
+): pass
