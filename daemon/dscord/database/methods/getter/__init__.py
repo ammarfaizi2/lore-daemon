10bfa4ce176410c249d29ac222e1ea15d754e04f
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-10-04 06:52:05 +0700

    discord: Add get_atom_urls() to get the list of atom URLs
    Add get_atom_urls() in the database getter directory to get the list of
    atom URLs. With this, we can display and manage them.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20221003235230.1824-5-kiizuha@gnuweeb.org
    Cc: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
    Cc: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Cc: GNU/Weeb Mailing List <gwml@vger.gnuweeb.org>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ daemon/dscord/database/methods/getter/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2022  Muhammad Rizki <kiizuha@gnuweeb.org>
+#
+
+
+from .get_atom_urls import GetAtomURL
+
+
+class Getter(
+	GetAtomURL
+): pass
