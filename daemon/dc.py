51aaf75c7036951de0942d8cddb07165a1473288
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-10-04 06:52:14 +0700

    discord: Use the created DB() class
    Use the DB() class into the __init__() function of the GWClient() class
    and add the MySQL connection to the GWClient() class parameter.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20221003235230.1824-14-kiizuha@gnuweeb.org
    Cc: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
    Cc: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Cc: GNU/Weeb Mailing List <gwml@vger.gnuweeb.org>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- daemon/dc.py
+++ daemon/dc.py
@@ -5,6 +5,7 @@
 
 import os
 from dotenv import load_dotenv
+from mysql import connector
 
 from dscord.gnuweeb import GWClient
 
@@ -12,7 +13,14 @@ from dscord.gnuweeb import GWClient
 def main():
 	load_dotenv("discord.env")
 
-	client = GWClient()
+	client = GWClient(
+		db_conn=connector.connect(
+			host=os.getenv("DB_HOST"),
+			user=os.getenv("DB_USER"),
+			password=os.getenv("DB_PASS"),
+			database=os.getenv("DB_NAME")
+		)
+	)
 	client.run(os.getenv("DISCORD_TOKEN"), log_handler=None)
 
 
--- daemon/dscord/gnuweeb/client.py
+++ daemon/dscord/gnuweeb/client.py
@@ -8,9 +8,12 @@ from discord.ext import commands
 from discord import Intents
 from dscord.config import ACTIVITY_NAME
 
+from dscord.database import DB
+
 
 class GWClient(commands.Bot):
-	def __init__(self) -> None:
+	def __init__(self, db_conn) -> None:
+		self.db = DB(db_conn)
 		intents = Intents.default()
 		intents.message_content = True
 		super().__init__(
