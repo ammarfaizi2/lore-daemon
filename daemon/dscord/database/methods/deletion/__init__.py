6479a667a38881b78fa5c7937faa05e8ea107e50
Muhammad Rizki <kiizuha@gnuweeb.org>
2022-10-04 06:52:10 +0700

    discord: Add delete_atom() in deletion directory
    Add delete_atom() for delete the specific atom URL.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20221003235230.1824-10-kiizuha@gnuweeb.org
    Cc: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
    Cc: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Cc: GNU/Weeb Mailing List <gwml@vger.gnuweeb.org>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

---
+++ daemon/dscord/database/methods/deletion/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: GPL-2.0-only
+#
+# Copyright (C) 2022  Muhammad Rizki <kiizuha@gnuweeb.org>
+#
+
+
+from .delete_atom import DeleteAtom
+
+
+class Deletion(
+	DeleteAtom,
+): pass
