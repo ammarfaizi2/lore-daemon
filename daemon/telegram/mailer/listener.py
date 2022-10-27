a89e588473a3ac7c3b8418879e67d39229659b06
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2022-10-27 21:51:34 +0700

    daemon: telegram: Handle MySQL error
    A previous patch creates a mechanism to allow the caller to reconnect
    to the database by calling db.connect(). Handle MySQL error in the
    main daemon loop path. Do reconnect to the database if such an error
    happens. This way, the daemon can automatically recover from the MySQL
    server restart without having the user restart the daemon.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- daemon/telegram/mailer/listener.py
+++ daemon/telegram/mailer/listener.py
@@ -7,6 +7,7 @@
 from pyrogram.types import Message
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from telegram.packages import DaemonClient
+from mysql import connector
 from atom import Scraper
 from atom import utils
 from enums import Platform
@@ -43,13 +44,35 @@ class Bot():
 		)
 
 
+	async def handle_db_error(self, e):
+		#
+		# TODO(ammarfaizi2):
+		# Ideally, we also want to log and report this situation.
+		#
+		print(f"Database error: {str(e)}")
+		print("Reconnecting to the database...")
+
+		#
+		# Don't do this too often to avoid reconnect burst.
+		#
+		delay_in_secs = 3
+		reconnect_attempts = 3
+
+		self.db.ping(reconnect=True, attempts=reconnect_attempts,
+			     delay=delay_in_secs)
+
+
 	async def __run(self):
 		print("[__run]: Running...")
-		for url in self.db.get_atom_urls():
-			try:
+		try:
+			for url in self.db.get_atom_urls():
 				await self.__handle_atom_url(url)
-			except:
-				print(traceback.format_exc())
+		except connector.errors.OperationalError as e:
+			await self.handle_db_error(e)
+		except connector.errors.DatabaseError as e:
+			await self.handle_db_error(e)
+		except:
+			print(traceback.format_exc())
 
 		if not self.isRunnerFixed:
 			self.isRunnerFixed = True
