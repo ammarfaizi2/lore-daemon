aae8fbb4dcc5a3c1827df70a6ed3763d9dc73619
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-01-19 00:53:53 +0700

    discord: Implement DaemonException and report_err in get_lore_mail.py
    In the last series, I didn't care so much to look into the Discord's
    get_lore_mail.py slash command. I've implemented them same as in the
    listener.py.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20230118175356.1853-15-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- daemon/dscord/gnuweeb/plugins/slash_commands/get_lore_mail.py
+++ daemon/dscord/gnuweeb/plugins/slash_commands/get_lore_mail.py
@@ -10,6 +10,7 @@ from discord import app_commands
 from atom import utils
 from atom import Scraper
 from enums import Platform
+from exceptions import DaemonException
 
 
 class GetLoreSC(commands.Cog):
@@ -29,9 +30,11 @@ class GetLoreSC(commands.Cog):
 			mail = await s.get_email_from_url(url)
 			text, _, is_patch = utils.create_template(mail, Platform.DISCORD)
 		except:
-			exc_str = utils.catch_err()
-			self.bot.logger.warning(exc_str)
-			await self.bot.send_log_file(url)
+			e = DaemonException()
+			e.set_thread_url(url)
+			e.set_message(utils.catch_err())
+			await self.bot.report_err(e)
+			return
 
 		if is_patch:
 			m = await self.bot.send_patch_mail_interaction(
