1701589a63270d2204d4386630e1cc35d910dd90
Muhammad Rizki <kiizuha@gnuweeb.org>
2023-01-19 00:53:40 +0700

    telegram: Simplify code to get DB_PORT from env
    Using `int(os.environ.get("DB_PORT", 3306))` is the best practice to get
    the DB_PORT rather than using if statement. This change simplifies the
    code.
    
    Signed-off-by: Muhammad Rizki <kiizuha@gnuweeb.org>
    Link: https://lore.gnuweeb.org/gwml/20230118175356.1853-2-kiizuha@gnuweeb.org
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- daemon/tg.py
+++ daemon/tg.py
@@ -22,12 +22,6 @@ def main():
 	logger = BotLogger()
 	logger.init()
 
-	port = os.getenv("DB_PORT")
-	if not port:
-		port = 3306
-	else:
-		port = int(port)
-
 	client = DaemonTelegram(
 		"telegram/storage/EmailScraper",
 		api_id=int(os.getenv("API_ID")),
@@ -37,7 +31,7 @@ def main():
 		conn=connector.connect(
 			host=os.getenv("DB_HOST"),
 			user=os.getenv("DB_USER"),
-			port=port,
+			port=int(os.environ.get("DB_PORT", 3306)),
 			password=os.getenv("DB_PASS"),
 			database=os.getenv("DB_NAME")
 		),
